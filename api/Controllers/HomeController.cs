ed74ee942f49b90e149a33117266900dc4bb500d
Allan Barcelos <allan@barcelos.dev>
2025-03-13 10:51:47 -0400

    authentication, authorization

---
+++ api/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+namespace API.Controllers {
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.AspNetCore.Authorization;
+
+    [ApiController]
+    [Route("")]
+    public class HomeController: ControllerBase
+    {
+        // htpp://localhost/
+        [HttpGet]
+        public IActionResult ApiIsWroking()
+        {
+            return Ok(new { Status = "Success", Message = "API is working"});
+        }
+
+        // htpp://localhost/protected
+        [HttpGet("protected")]
+        [Authorize(Policy = "RequireUserRole")]
+        public IActionResult ProtectedEndpoint()
+        {
+            return Ok(new { Status = "Success", Message = "If you are logged, you can see me"});
+        }
+
+    }
+}
